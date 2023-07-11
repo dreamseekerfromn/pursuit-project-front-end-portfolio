461ba097e19d221cd9951e29b2f9e123430ea289
Sung Yi <sy@Sungs-Air.fios-router.home>
2023-07-10 22:59:02 -0400

    still working for the skeleton

---
+++ index.js
@@ -0,0 +1,15 @@
+const url = "https://mhw-db.com/monsters/";
+const submitB = document.querySelector('button');
+
+function onSubmit(e){
+    e.preventDefault();
+    //https://mhw-db.com/monsters?q={"name":"Bazelgeuse"}
+    fetch(``)
+}
+
+function createContainer(e){
+    const div = document.createElement("div");
+    const article = document.createElement("article");
+    const image = document.createElement("img");
+
+}
