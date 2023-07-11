fc42c09fb71fc08a95dc237ceecbfc43c1ac7585
Sung Yi <sy@Sungs-Air.fios-router.home>
2023-07-11 03:02:17 -0400

    still working for basic skeleton design, NO CSS

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const url = "https://mhw-db.com/monsters/";
-const submitB = document.querySelector('button');
+const submitB = document.querySelector('#new_item_submit');
+const err = document.querySelector('.err');
+
+submitB.addEventListener('click', e=>onSubmit(e));
 
 function onSubmit(e){
     e.preventDefault();
     //https://mhw-db.com/monsters?q={"name":"Bazelgeuse"}
-    fetch(``)
+    //fetch(``)
+    const monsterBox = document.querySelector('.monster_list');
+    const monsterName = monsterBox.options[monsterBox.selectedIndex].text;
+    fetch(`${url}\?q={\"name\":\"${monsterName}\"}`).then(data => data.json()).then(json => jsonHandler(json));
+    createContainer(e);
 }
 
 function createContainer(e){
     const div = document.createElement("div");
     const article = document.createElement("article");
     const image = document.createElement("img");
+    const p = document.createElement('p');
 
+
+
+}
+
+function jsonHandler(json){
+    console.log(json);
 }
